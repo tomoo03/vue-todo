7725ad1f2549234326a620b87488d69c7332f3af
t_omo_o_03 <shishimarureo@gmail.com>
2021-01-23 00:51:59 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>TODOリスト</h1>
+    <div>
+      <input v-model="newTodoName">
+      <button @click="addTodo()">追加</button>
+    </div>
+
+    <ul>
+      <li v-for="(todo, key) in todos" :key="key">
+        <input type="checkbox" v-model="todo.isComplete" @click="updateTodo(todo, key)">
+         {{ todo.name }}
+         <button @click="removeTodo(key)">X</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firebase from 'firebase'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      db: null,
+      todosRef: null,
+      newTodoName: '',
+      todos: {}
+    }
+  },
+  created() {
+    this.db = firebase.firestore();
+    this.todosRef = this.db.collection('todos');
+    this.todosRef.onSnapshot(querySnapshot => {
+      const obj = {};
+      querySnapshot.forEach(doc => {
+        obj[doc.id] = doc.data();
+      });
+      this.todos = obj;
+    });
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoName === '') {
+        return;
+      }
+      this.todosRef.add({
+        name: this.newTodoName,
+        isComplete: false
+      });
+    },
+    updateTodo(todo, key) {
+      todo.isComplete = !todo.isComplete;
+      this.todosRef.doc(key).update(todo);
+    },
+    removeTodo(key) {
+      this.todosRef.doc(key).delete();
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA3LQ3KLmSKyFAtO902vZX6O2mBmbHJJcE",
+  authDomain: "vue-todo-204b0.firebaseapp.com",
+  projectId: "vue-todo-204b0",
+  storageBucket: "vue-todo-204b0.appspot.com",
+  messagingSenderId: "1076866271712",
+  appId: "1:1076866271712:web:b63dc32ae12ee3fe954385"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
